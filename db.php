996ab706bcf6ee70c4a2ded5a35ec9a5b989f0fa
Adhi Purnawan <adhi.instiki@gmail.com>
2024-11-29 20:22:59 +0800

    Update

--- db.php
+++ db.php
@@ -1,15 +1,12 @@
 <?php
-// db.php
-$db_url = getenv('mysql://cnsbyfdh2qp6sdl2:qlev2fbx1qt5qmu6@o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/qfhwap8byb54498t'); 
-$db_parts = parse_url($db_url);
-
-$host = $db_parts['host'];
-$user = $db_parts['user'];
-$password = $db_parts['pass'];
-$db_name = ltrim($db_parts['path'], '/');
+$host = 'localhost'; 
+$user = 'root';      
+$password = '';     
+$db_name = 'company_profile'; 
 
 $conn = new mysqli($host, $user, $password, $db_name);
 
+// Periksa koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
