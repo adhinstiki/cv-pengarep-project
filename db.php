bb7bb5a517adf1ccd72217b69668def73ddd3482
Adhi Purnawan <adhi.instiki@gmail.com>
2024-12-02 22:09:44 +0800

    Update db.php

--- db.php
+++ db.php
@@ -3,11 +3,19 @@ $host = 'o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $user = 'cnsbyfdh2qp6sdl2';      
 $password = 'qlev2fbx1qt5qmu6';     
 $db_name = 'qfhwap8byb54498t'; 
+$charset = 'utf8mb4';
 
-$conn = new mysqli($host, $user, $password, $db_name);
+$dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
-// Periksa koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+    echo "Connection successful!";
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
