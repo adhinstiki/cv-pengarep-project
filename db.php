8d9a8fd1b99a98d6553afb976411029bbe2094df
Adhi Purnawan <adhi.instiki@gmail.com>
2025-01-10 03:32:52 +0800

    Update db.php

--- db.php
+++ db.php
@@ -6,8 +6,12 @@ $db_name = 'db_cvpengarep';
 $charset = 'utf8mb4';
 
 try {
+    // Membuat koneksi PDO ke database Cloud SQL
     $pdo = new PDO("mysql:dbname=$dbname;unix_socket=$host", $user, $password);
+    // Set error mode untuk menangani kesalahan
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
+    // Jika koneksi gagal, tampilkan pesan kesalahan
     echo "Connection failed: " . $e->getMessage();
 }
 ?>
