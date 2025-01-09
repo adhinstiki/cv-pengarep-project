c8d7836f77c1750ca26238b53529fe4647ee7bec
Adhi Purnawan <adhi.instiki@gmail.com>
2025-01-10 02:27:53 +0800

    Update db.php

--- db.php
+++ db.php
@@ -10,7 +10,6 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
-    PDO::ATTR_TIMEOUT            => 10,
 ];
 
 try {
