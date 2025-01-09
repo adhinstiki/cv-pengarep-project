28c5a40337cbc5787b716acd0b8091fb5b0fef77
Adhi Purnawan <adhi.instiki@gmail.com>
2025-01-10 03:30:13 +0800

    Update db.php

--- db.php
+++ db.php
@@ -5,17 +5,9 @@ $password = 'adhi1234';
 $db_name = 'db_cvpengarep'; 
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-    PDO::ATTR_TIMEOUT            => 30,
-];
-
 try {
-    $pdo = new PDO($dsn, $user, $password, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    $pdo = new PDO("mysql:dbname=$dbname;unix_socket=$host", $user, $password);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
